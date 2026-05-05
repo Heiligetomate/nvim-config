622d4843c3230643cd5a2c6ec06d4dee5c8d2227
Heiligetomate <heiligetomate@protonmail.com>
2026-05-05 10:20:19 +0200

    add markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,51 +1,57 @@
 return {
-  {
-  "stevearc/conform.nvim",
-  lazy = false,
-  opts = require "configs.conform",
-},
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-  "ellisonleao/gruvbox.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("gruvbox").setup({})
-    vim.cmd("colorscheme gruvbox")
-  end,
-},
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    ft = { "markdown" },
-    opts = {},
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      highlight = {
-        enable = false,
-      },
-      indent = {
-        enable = true,
-      },
-    },
-  },
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		opts = require("configs.conform"),
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({})
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		ft = { "markdown" },
+		opts = {},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			highlight = {
+				enable = false,
+			},
+			indent = {
+				enable = true,
+			},
+		},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = { "markdown" },
+		build = "bash -c 'cd app && npm install'",
+		cmd = { "MarkdownPreview" },
+	},
 }
