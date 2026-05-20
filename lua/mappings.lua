c094d4010c52ce8d16eeb9453009fa78de55c028
Heiligetomate <heiligetomate@protonmail.com>
2026-05-20 12:37:07 +0200

    moving stuff

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,36 +1,47 @@
-require("nvchad.mappings")
+require "nvchad.mappings"
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+map("n", "<S-Down>", ":m .+1<CR>==")
+map("n", "<S-Up>", ":m .-2<CR>==")
+map("v", "<S-Down>", ":m '>+1<CR>gv=gv")
+map("v", "<S-Up>", ":m '<-2<CR>gv=gv")
+
+map("n", "<S-Right>", ">>")
+map("n", "<S-Left>", "<<")
+map("v", "<S-Right>", ">gv")
+map("v", "<S-Left>", "<gv")
+
 vim.keymap.set("n", "<Esc>", function()
-	vim.lsp.buf.clear_references()
-	for _, win in ipairs(vim.api.nvim_list_wins()) do
-		if vim.api.nvim_win_get_config(win).relative ~= "" then
-			vim.api.nvim_win_close(win, true)
-		end
-	end
+  vim.lsp.buf.clear_references()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative ~= "" then
+      vim.api.nvim_win_close(win, true)
+    end
+  end
 end)
 
 vim.keymap.set("n", "<leader>cy", function()
-	local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
-	if #diagnostics == 0 then
-		return
-	end
-	local msg = table.concat(
-		vim.tbl_map(function(d)
-			return d.message
-		end, diagnostics),
-		"\n"
-	)
-	vim.fn.setreg("+", msg)
-	print("Copied diagnostic: " .. msg)
+  local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line "." - 1 })
+  if #diagnostics == 0 then
+    return
+  end
+  local msg = table.concat(
+    vim.tbl_map(function(d)
+      return d.message
+    end, diagnostics),
+    "\n"
+  )
+  vim.fn.setreg("+", msg)
+  print("Copied diagnostic: " .. msg)
 end, { desc = "Copy diagnostic message" })
 
 vim.keymap.set("n", "<leader>u", function()
-	local word = vim.fn.expand("<cWORD>")
-	vim.fn.jobstart({ "xdg-open", word }, { detach = true })
+  local word = vim.fn.expand "<cWORD>"
+  vim.fn.jobstart({ "xdg-open", word }, { detach = true })
 end, { desc = "Open URL under cursor" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>noh<cr>", { desc = "Clear search highlights" })
@@ -44,5 +55,5 @@ vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "New tab" })
 vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close tab" })
 
 for i = 1, 9 do
-	vim.keymap.set("n", "<leader>t" .. i, i .. "gt", { desc = "Go to tab " .. i })
+  vim.keymap.set("n", "<leader>t" .. i, i .. "gt", { desc = "Go to tab " .. i })
 end
