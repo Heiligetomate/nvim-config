0281ddf3ac0a18d7031c8983c50be79646a9eca8
Heiligetomate <heiligetomate@protonmail.com>
2026-05-22 08:33:16 +0200

    git diff

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 local map = vim.keymap.set
 
@@ -19,45 +19,45 @@ map("n", "<leader>cs", "<cmd>Cheatsheet<cr>", { desc = "Cheatsheet" })
 
 map("n", "<leader>n", "<Nop>")
 map("n", "<leader>nn", function()
-  require("notify").dismiss { silent = true, pending = true }
+	require("notify").dismiss({ silent = true, pending = true })
 end, { desc = "Clear notifications" })
 
 map("n", "<leader>nc", function()
-  local history = require("notify").history()
-  local last = history[#history]
-  if last then
-    vim.fn.setreg("+", table.concat(last.message, "\n"))
-    vim.notify("Copied!", vim.log.levels.INFO)
-  end
+	local history = require("notify").history()
+	local last = history[#history]
+	if last then
+		vim.fn.setreg("+", table.concat(last.message, "\n"))
+		vim.notify("Copied!", vim.log.levels.INFO)
+	end
 end, { desc = "Copy last notification" })
 
 vim.keymap.set("n", "<Esc>", function()
-  vim.lsp.buf.clear_references()
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    if vim.api.nvim_win_get_config(win).relative ~= "" then
-      vim.api.nvim_win_close(win, true)
-    end
-  end
+	vim.lsp.buf.clear_references()
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		if vim.api.nvim_win_get_config(win).relative ~= "" then
+			vim.api.nvim_win_close(win, true)
+		end
+	end
 end)
 
 vim.keymap.set("n", "<leader>cy", function()
-  local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line "." - 1 })
-  if #diagnostics == 0 then
-    return
-  end
-  local msg = table.concat(
-    vim.tbl_map(function(d)
-      return d.message
-    end, diagnostics),
-    "\n"
-  )
-  vim.fn.setreg("+", msg)
-  print("Copied diagnostic: " .. msg)
+	local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+	if #diagnostics == 0 then
+		return
+	end
+	local msg = table.concat(
+		vim.tbl_map(function(d)
+			return d.message
+		end, diagnostics),
+		"\n"
+	)
+	vim.fn.setreg("+", msg)
+	print("Copied diagnostic: " .. msg)
 end, { desc = "Copy diagnostic message" })
 
 vim.keymap.set("n", "<leader>u", function()
-  local word = vim.fn.expand "<cWORD>"
-  vim.fn.jobstart({ "xdg-open", word }, { detach = true })
+	local word = vim.fn.expand("<cWORD>")
+	vim.fn.jobstart({ "xdg-open", word }, { detach = true })
 end, { desc = "Open URL under cursor" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>noh<cr>", { desc = "Clear search highlights" })
@@ -70,6 +70,10 @@ vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "New tab" })
 vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close tab" })
 
+vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<cr>", { desc = "Open diff view" })
+
+vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<cr>", { desc = "Close diff view" })
+
 for i = 1, 9 do
-  vim.keymap.set("n", "<leader>t" .. i, i .. "gt", { desc = "Go to tab " .. i })
+	vim.keymap.set("n", "<leader>t" .. i, i .. "gt", { desc = "Go to tab " .. i })
 end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,87 +1,92 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    lazy = false,
-    opts = require "configs.conform",
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup {}
-      vim.cmd "colorscheme gruvbox"
-    end,
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    ft = { "markdown" },
-    opts = {},
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      highlight = {
-        enable = false,
-      },
-      indent = {
-        enable = true,
-      },
-    },
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    ft = { "markdown" },
-    build = "bash -c 'cd app && npm install'",
-    cmd = { "MarkdownPreview" },
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = "Trouble",
-    config = function()
-      require("trouble").setup {
-        position = "float",
-        focus = true,
-        win = {
-          type = "float",
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		opts = require("configs.conform"),
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({})
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		ft = { "markdown" },
+		opts = {},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			highlight = {
+				enable = false,
+			},
+			indent = {
+				enable = true,
+			},
+		},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = { "markdown" },
+		build = "bash -c 'cd app && npm install'",
+		cmd = { "MarkdownPreview" },
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = "Trouble",
+		config = function()
+			require("trouble").setup({
+				position = "float",
+				focus = true,
+				win = {
+					type = "float",
 
-          border = "rounded",
-          size = { width = 0.8, height = 0.4 },
-          position = { 0.5, 0.5 },
-        },
-      }
-    end,
-  },
-  {
-    "sudormrfbin/cheatsheet.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    opts = {},
-  },
-  {
-    "rcarriga/nvim-notify",
-    lazy = false,
-    config = function()
-      vim.notify = require "notify"
-    end,
-  },
+					border = "rounded",
+					size = { width = 0.8, height = 0.4 },
+					position = { 0.5, 0.5 },
+				},
+			})
+		end,
+	},
+	{
+		"sudormrfbin/cheatsheet.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		opts = {},
+	},
+	{
+		"rcarriga/nvim-notify",
+		lazy = false,
+		config = function()
+			vim.notify = require("notify")
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		lazy = false,
+		config = function() end,
+	},
 }
